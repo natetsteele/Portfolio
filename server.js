bd5d78cd56300596ca576c8915147cee0ec7af77
Nathan Steele <natetsteele@gmail.com>
2018-11-06 12:48:50 -0600

    Initialized server

--- server.js
+++ server.js
@@ -4,12 +4,11 @@ const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/Portfolio'));
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/Portfolio/index.html'));
+app.get('*', (req, res) => {
+    res.sendFile(`./front-end/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
 });
 
 // Start the app by listening on the default Heroku port
